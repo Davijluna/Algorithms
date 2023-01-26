fa322a5f838e6fdbe8f9453a0a400269113a71f4
davi <davijluna@gmail.com>
2023-01-26 19:49:33 -0300

    feat: implementando função study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,17 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    # raise NotImplementedError
+    list = 0
+    if target_time is None:
+        return None
+    try:
+        for index, hours in permanence_period:
+            if index <= target_time <= hours:
+                list += 1
+        return list
+    except TypeError:
+        return None
+
+
+#  if target_time <= 0 or not target_time:
+#             return None
