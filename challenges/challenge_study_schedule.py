aaf70178b17f710058ed1c7f599a7f597aaef76c
davi <davijluna@gmail.com>
2023-01-26 19:53:15 -0300

    refactor: diminuindo complexidades

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,8 +2,6 @@ def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
     # raise NotImplementedError
     list = 0
-    if target_time is None:
-        return None
     try:
         for index, hours in permanence_period:
             if index <= target_time <= hours:
