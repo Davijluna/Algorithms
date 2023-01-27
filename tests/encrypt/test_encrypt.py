1c702dccb214323c6644f20e88cb402b772db0f3
davi <davijluna@gmail.com>
2023-01-27 12:24:11 -0300

    test: implementando test de arquivo test_encrypt.py

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,15 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+
+    # pass
+    assert encrypt_message("testes", -1) == "setset"
+    assert encrypt_message("testes", 1) == "t_setse"
+    assert encrypt_message("testes", 2) == "sets_et"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("testes", "string")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1, 1)
