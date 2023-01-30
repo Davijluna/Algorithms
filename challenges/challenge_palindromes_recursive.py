3c6313eb6bcfe849b6d9a3099136f7e9f0bdf870
davi <davijluna@gmail.com>
2023-01-30 10:29:40 -0300

    feat: requisito 3 implementando função is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,23 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+
+    string = word
+    # stringIndex = low_index
+    # StringHigh = high_index
+
+    if not string:
+        return False
+    if string[low_index] != string[high_index]:
+        return False
+    if low_index >= high_index or low_index == high_index:
+        return True
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    # try:
+    # stringSemEspaços = string.replace(' ', '')
+    # stringTodaMinuscula = stringSemEspaços.lower()
+    # stringInvertida = stringTodaMinuscula[::-1]
+    # if stringInvertida == stringTodaMinuscula:
+    #     return True
+    # else:
+    #     return False
+    # except TypeError:
+    #     return False
