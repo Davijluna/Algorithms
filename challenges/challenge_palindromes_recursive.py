1e28cb1b592bb257f52a9d488db90f2c006cd97e
davi <davijluna@gmail.com>
2023-02-02 16:03:41 -0300

    feactor: corrigindo linha de erro

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -9,4 +9,3 @@ def is_palindrome_recursive(word, low_index, high_index):
     if low_index >= high_index or low_index == high_index:
         return True
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
-
